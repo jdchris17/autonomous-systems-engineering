e331564e0bcb849c1599c86d7a1e9dfc24fc2453
Jake Darrah <jake@enduria.co>
2026-07-16 13:07:41 -0400

    Complete ENGR500 Module 2 variables and data types

---
+++ courses/ENGR500/engineering_profile.py
@@ -0,0 +1,6 @@
+project_name = "Autonomous Systems Engineering"
+language = "Python"
+version = 1.0
+repository = "Github"
+completed_modules = 2
+print(f"The project name is: {project_name}. The programming language is {language}. The version is {version}. The respository lives on {repository}. How far I've gotten thus far is: {completed_modules} modules completed.")
---
+++ courses/ENGR500/navigation_constants.py
@@ -0,0 +1,4 @@
+earth_radius_km = 6371
+speed_of_light = 299792458
+gps_enabled = False
+camera_fov = 77.0
